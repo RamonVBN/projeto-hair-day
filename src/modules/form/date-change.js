6a0ab9d6c49ea1ac56f32c5698ca8a26081e4c95
RamonVBN <141188009+RamonVBN@users.noreply.github.com>
2025-02-24 16:48:27 -0300

    Tentando fazer funcionar no github 2

--- src/modules/form/date-change.js
+++
@@ -1,9 +0,0 @@
-import {schedulesDay} from "../schedules/load"
-
-// Seleciona o input de data
-
-const selectedDate = document.getElementById('date')
-
-// Recarrega a lista de horários quando o input de data mudar.
-selectedDate.onchange = () => schedulesDay()
-
