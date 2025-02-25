0b4eb306f916dfd19edafe978faf8da13ee101c6
Ruan Barros <ruan.personal@peppo.dev>
2025-02-25 00:20:02 -0300

    add env to control api request

--- src/services/schedule-cancel.js
+++ src/services/schedule-cancel.js
@@ -2,17 +2,21 @@ import { fakeDatabase } from "../main.js";
 import { apiConfig } from "./api-config.js";
 
 export async function scheduleCancel({id}){
+    if(!process.env.SHOULD_MAKE_API_CALL){
+        const indexToRemove = fakeDatabase.findIndex((value)=> value.id===id)
+        fakeDatabase.splice(indexToRemove, 1)
+        alert('Agendamento cancelado com sucesso!')
+        return
+    }
+
     try {
         await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
             method: 'DELETE'
         })
-
         alert('Agendamento cancelado com sucesso!')
         
     } catch (error) {
         console.log(error)
-        // alert('Não foi possível cancelar o agendamento.')
-        const indexToRemove = fakeDatabase.findIndex((value)=> value.id===id)
-        fakeDatabase.splice(indexToRemove, 1)
+        alert('Não foi possível cancelar o agendamento.')
     }
 }
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -2,6 +2,12 @@ import { fakeDatabase } from "../main.js";
 import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({id, name, when}) {
+    if(!process.env.SHOULD_MAKE_API_CALL){
+        fakeDatabase.push({id,name, when})
+        alert('Agendamento realizado com sucesso!')
+        return
+    }
+
     try {
         // Fazendo a requisição.
         await fetch(`${apiConfig.baseURL}/schedules`, {
@@ -15,7 +21,6 @@ export async function scheduleNew({id, name, when}) {
         alert('Agendamento realizado com sucesso!')
     } catch (error) {
         console.log(error)
-        // alert('Não foi possível agendar, tente novamente mais tarde.')
-        fakeDatabase.push({id,name, when})
+        alert('Não foi possível agendar, tente novamente mais tarde.')
     }
 }
